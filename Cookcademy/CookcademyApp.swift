d3dd259f7802dfbe4f74bb954d715e8899e2bc5b
Kevin Martinez <82669898+kadm91@users.noreply.github.com>
2023-12-26 22:55:52 -0700

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Kevin Martinez on 12/26/23.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
