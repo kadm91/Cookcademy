b25007a199b3010a91c04262ae7197a409a6dfc5
Kevin Martinez <82669898+kadm91@users.noreply.github.com>
2023-12-31 16:56:05 -0700

    Create the category view and add images to the asset folder

---
+++ Cookcademy/Views/RecipeCategoryGridView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeCategoryGridView.swift
+//  Cookcademy
+//
+//  Created by Kevin Martinez on 12/31/23.
+//
+
+import SwiftUI
+
+struct RecipeCategoryGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeCategoryGridView()
+}
