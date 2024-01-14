5e20ca5a9bf2215920d29285139633ebe63c2896
Kevin Martinez <82669898+kadm91@users.noreply.github.com>
2024-01-13 17:01:21 -0700

    Move init() to styling navegationTitle to MainTabView

--- Cookcademy/Views/MainTabView.swift
+++ Cookcademy/Views/MainTabView.swift
@@ -24,6 +24,29 @@ struct MainTabView: View {
       }
         
     }
+    
+    
+    init() {
+        UISegmentedControl.appearance().selectedSegmentTintColor = .customForeground
+        UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor: UIColor.cusumBackground], for: .selected)
+        UISegmentedControl.appearance().backgroundColor = .cusumBackground
+        UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor: UIColor.customForeground], for: .normal)
+        
+        
+        UIStepper.appearance().backgroundColor = .cusumBackground
+        UIStepper.appearance().setDecrementImage(UIImage(systemName: "minus")?.withTintColor(.accent), for: .normal)
+        UIStepper.appearance().setIncrementImage(UIImage(systemName: "plus")?.withTintColor(.accent), for: .normal)
+        
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.customForeground, .font: UIFont(name: "Georgia-Bold", size: 30) as Any]
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.customForeground]
+        
+        
+    }
+    
+    
+    
+    
+    
 }
 
 #Preview {
